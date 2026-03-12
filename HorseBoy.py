3e21e28f5fd292775a3555f7773e84238681bc81
OrestesNZ <74434078+OrestesNZ@users.noreply.github.com>
2026-03-12 21:27:21 +1300

    Initial commit

---
+++ HorseBoy.py
@@ -0,0 +1,11 @@
+#suck horse cum
+
+# ctrl+shift+p, git: commit, commit
+# save fucking file, ctrl+s 
+# ctrl+SHAFT+p, git:push, save, commit
+# ctrl+s 
+
+print("horse cum contains macro and micro nutrients")
+
+if 2 * 2 != 5:
+    print("horse cum is not 5")
